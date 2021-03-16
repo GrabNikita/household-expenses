4b56755d4a2219082a2aba7412905c8d992d897b
Grab Nikita <grab.nikita.syktyvkar@gmail.com>
2021-03-16 18:14:49 +0300

    create market resource

---
+++ resources/views/markets.blade.php
@@ -0,0 +1,11 @@
+<?
+/**
+ * @var Illuminate\Database\Eloquent\Collection $markets
+ */
+?>
+
+@extends('layouts.app')
+
+@section('content')
+    <h1>Магазины</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('markets', Controllers\MarketController::class);
